6156ee75baead9ed1e0a769549d6d40afd0a4360
Matt Stauffer <matt@tighten.co>
2025-01-09 13:33:39 -0500

    Add preview on create page

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
 TODO:
-MUST DO:
-- add at least one video and build the css around it to make the show page look great
-
 NICE TO HAVE:
 - Cache
 - Serve video from CDN
 - add analytics, including some event so we can track number of broadcasts created without having to keep them in the db forever
 - make a cute pdf they can print out and put on the fridge?
 - build an actual design
-- preview the tv when you're making a broadcast
 - create a more helpful 404 page
--- resources/views/livewire/create-broadcast.blade.php
+++ resources/views/livewire/create-broadcast.blade.php
@@ -1,31 +1,49 @@
-<div class="px-4 pt-8 pb-4">
-    <form wire:submit="save">
-        <div class="mb-8">
-            <label class="font-bold" for="school_district">Name of school district</label><br>
-            <input wire:model="schoolDistrict" id="school_district" placeholder="Atlanta Public Schools">
-            @error('schoolDistrict')<div class="text-red-500">{{ $message }}</div>@enderror
-        </div>
+<div>
+    <div class="px-4 pt-8 pb-4">
+        <form wire:submit="save">
+            <div class="mb-8">
+                <label class="font-bold" for="school_district">Name of school district</label><br>
+                <input wire:model.live="schoolDistrict" id="school_district" placeholder="Atlanta Public Schools">
+                @error('schoolDistrict')<div class="text-red-500">{{ $message }}</div>@enderror
+            </div>
 
-        <div class="mb-8">
-            <label class="font-bold" for="canceled">Canceled or not</label><br>
-            <input type="radio" wire:model="canceled" id="canceled-true" value="1" checked> <label for="canceled-true">Canceled</label>
-            <input type="radio" wire:model="canceled" id="canceled-false" value="0" class="ml-4"> <label for="canceled-false">Not Canceled</label>
-            @error('canceled')<div class="text-red-500">{{ $message }}</div>@enderror
-        </div>
+            <div class="mb-8">
+                <label class="font-bold" for="canceled">Canceled or not</label><br>
+                <input type="radio" wire:model.live="canceled" id="canceled-true" value="1" checked> <label for="canceled-true">Canceled</label>
+                <input type="radio" wire:model.live="canceled" id="canceled-false" value="0" class="ml-4"> <label for="canceled-false">Not Canceled</label>
+                @error('canceled')<div class="text-red-500">{{ $message }}</div>@enderror
+            </div>
 
-        <div class="mb-8">
-            <label class="font-bold" for="date">Broadcast date</label><br>
-            <input type="date" wire:model="date" id="date">
-            @error('date')<div class="text-red-500">{{ $message }}</div>@enderror
-        </div>
+            <div class="mb-8">
+                <label class="font-bold" for="date">Broadcast date</label><br>
+                <input type="date" wire:model.live="date" id="date">
+                @error('date')<div class="text-red-500">{{ $message }}</div>@enderror
+            </div>
 
-        <div class="mb-8">
-            <label class="font-bold" for="shortname">URL Shortname</label><br>
-            {{ url('') }}/d/<input wire:model="shortname" id="shortname" placeholder="aps" class="w-32">
-            <p class="text-gray-500 text-sm">Note: Once your date is past, we'll delete this broadcast, freeing up this shortname to be used again in the future.</p>
-            @error('shortname')<div class="text-red-500">{{ $message }}</div>@enderror
-        </div>
+            <div class="mb-8">
+                <label class="font-bold" for="shortname">URL Shortname</label><br>
+                {{ url('') }}/d/<input wire:model="shortname" id="shortname" placeholder="aps" class="w-32">
+                <p class="text-gray-500 text-sm">Note: Once your date is past, we'll delete this broadcast, freeing up this shortname to be used again in the future.</p>
+                @error('shortname')<div class="text-red-500">{{ $message }}</div>@enderror
+            </div>
 
-        <button type="submit" class="border px-3 py-2 hover:bg-blue-100 hover:cursor-pointer font-bold">Create broadcast</button>
-    </form>
+            <button type="submit" class="border px-3 py-2 hover:bg-blue-100 hover:cursor-pointer font-bold">Create broadcast</button>
+        </form>
+    </div>
+
+    <div class="mt-6 border-t -mx-6">
+        <div class="my-4 text-gray-500 ml-6">PREVIEW:</div>
+
+        <marquee class="select-none bg-red-700 text-white px-3 py-1 block">
+            @php
+                $carbonDate = \Carbon\Carbon::parse($date);
+            @endphp
+            {{ strtoupper($schoolDistrict ?: '(your school district)') }} UPDATE:
+            @if ($canceled)
+                School has been canceled because of snow for {{ $carbonDate->format('F j, Y') }}! Turn on the cartoons!
+            @else
+                Sorry, kids, school has <strong>not</strong> been canceled for {{ $carbonDate->format('F j, Y') }}.
+            @endif
+        </marquee>
+    </div>
 </div>
