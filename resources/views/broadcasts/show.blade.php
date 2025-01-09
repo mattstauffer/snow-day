2529e6045201f81b61fbb38bc3795ecccd4cb2a5
Matt Stauffer <matt@tighten.co>
2025-01-09 18:53:23 -0500

    Fix show title

--- resources/views/broadcasts/show.blade.php
+++ resources/views/broadcasts/show.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', $broadcast->school_district . ' - Snow Day Broadcast') }}</title>
+        <title>{{ $broadcast->school_district . ' - Snow Day Broadcast' }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
