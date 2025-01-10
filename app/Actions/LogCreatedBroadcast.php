3a46bcacbaab1bbee9b40a12c2fe3f0fb4e5c32b
Matt Stauffer <matt@tighten.co>
2025-01-09 22:14:01 -0500

    Fight and then give up on Fathom events

--- app/Actions/LogCreatedBroadcast.php
+++ app/Actions/LogCreatedBroadcast.php
@@ -11,14 +11,16 @@ class LogCreatedBroadcast
 {
     public function __invoke(Broadcast $broadcast)
     {
+        // I think I may have misunderstood what "create event" means in Fathom land. Pausing on this for now.
+        return;
         // @todo: move this into a job that can be retried on its own system later
 
         try {
-            Http::withToken(config('services.fathom.key'))->post('https://api.usefathom.com/v1/sites/' . config('services.fathom.id') . '/events', [
-                'school district' => $broadcast->school_district,
+            $response = Http::withToken(config('services.fathom.key'))->post('https://api.usefathom.com/v1/sites/' . config('services.fathom.id') . '/events', [
+                'name' => 'Create broadcast'
             ]);
         } catch (Throwable $e) {
-            Log::error('Failed pushing "created broadcast" event to Fathom. Broadcast: ' . $broadcast->school_district);
+            Log::error('Failed pushing "created broadcast" event to Fathom. Broadcast: ' . $broadcast->school_district . '. Message: ' . $e->getMessage());
         }
     }
 }
